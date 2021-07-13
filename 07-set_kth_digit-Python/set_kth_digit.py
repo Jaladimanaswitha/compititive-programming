b497f5ec2214f558356df1c00b1e9d1cb01cc65a
manaswitha <manaswithajaladi28@msitprogram.net>
2021-07-13 18:22:32 +0530

    done

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,32 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	flag=True
+	if(n<0):
+		n=-(n)
+		flag=False
+	a=[]
+	b=k
+	while(k>=0):
+		if(k!=0):
+			a.append(n%10)
+		n=n//10
+		k=k-1
+	# print(n)
+	n=(n*10)+d
+	for i in range(len(a)-1,-1,-1):
+		n=(n*10)+a[i]
+		# print(n)
+		# b=b-1
+	# print(n)
+	if(flag):
+		return (int(n))
+	else:
+		return -(int(n))
+
+fun_set_kth_digit(468,2,1)
+	
+	
+
+
 
