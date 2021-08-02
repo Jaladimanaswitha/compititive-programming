267f836ff7aedb0a103802a7f0f318ba2b1b7a4a
manaswitha <manaswithajaladi28@msitprogram.net>
2021-08-02 12:02:51 +0530

    done

--- 03-nth_pronicnumber-Python/nthpronicnumber.py
+++ 03-nth_pronicnumber-Python/nthpronicnumber.py
@@ -5,4 +5,16 @@
 
 def nthpronicnumber(n):
 	# Your code goes here
-	pass
+	res=[]
+	i=0
+	while(len(res)<=n):
+		for j in range(-1,i+1):
+			if(j*(j+1)==i):
+				res.append(i)
+				break
+		i+=1
+	return res[n]
+	print(res[n])
+
+
+# nthpronicnumber(13)
